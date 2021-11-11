91d7f0d73dee8062d799afee04b9f7eeb7fec553
Ging0423 <a8258993390@gmail.com>
2021-11-11 23:55:33 +0800

    UPDATE

--- src/main/java/loop/config/WebAppConfig.java
+++ src/main/java/loop/config/WebAppConfig.java
@@ -1,5 +1,8 @@
 package loop.config;
 
+import org.hibernate.SessionFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebAppConfig implements WebMvcConfigurer {
 
+	
+	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		//registry.addViewController("/login/page").setViewName("login");
 		
 	}
+	
+	
 	
 }
--- src/main/java/loop/item/yarnItem/service/YarnItemService.java
+++ src/main/java/loop/item/yarnItem/service/YarnItemService.java
@@ -3,7 +3,9 @@ package loop.item.yarnItem.service;
 import java.util.List;
 import java.util.Random;
 
-import org.hibernate.Session;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +16,19 @@ import loop.item.yarnItem.model.YarnItemDao;
 @Service
 @Transactional
 public class YarnItemService {
-	
+
 	@Autowired
 	YarnItemDao Dao;
+
+//	private SessionFactory hibernateFactory;
+
+//	@Autowired
+//	public YarnItemService(EntityManagerFactory factory) {
+//		if (factory.unwrap(SessionFactory.class) == null) {
+//			throw new NullPointerException("factory is not a hibernate factory");
+//		}
+//		this.hibernateFactory = factory.unwrap(SessionFactory.class);
+//	}
 
 	public String getRandomString(Integer length) {
 		String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
