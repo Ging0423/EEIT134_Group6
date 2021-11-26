cd229388117bdc549c6e6e47a903aa6cf54a5c7e
SyuRongSiou <xiu730401@hotmail.com>
2021-11-26 11:36:28 +0800

    20211126
    1) book 前端單一產品畫面
    2) kits CRUD

--- src/main/java/loop/item/booksItem/model/BooksItemRepository.java
+++ src/main/java/loop/item/booksItem/model/BooksItemRepository.java
@@ -4,4 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BooksItemRepository extends JpaRepository<BooksItemBean, Integer> {
 
+<<<<<<< Updated upstream
+=======
+	void update(BooksItemBean booksItemBean);
+
+>>>>>>> Stashed changes
 }
