d0353e7755783e1a9f68f284b4b25fa62ec83e7e
GoatOps <51600097+Ging0423@users.noreply.github.com>
2021-12-06 06:09:19 +0000

    UPDATE

--- src/main/java/loop/ecpay/service/PayementService.java
+++ src/main/java/loop/ecpay/service/PayementService.java
@@ -60,8 +60,8 @@ public class PayementService {
 		aioCheck.setMerchantTradeNo("loop" + s + ob.getOrderId());
 		
 		/* 付款完成通知回傳網址：不瞭解此網址的作用 */
-		aioCheck.setReturnURL("https://eeit13428loop.southeastasia.cloudapp.azure.com:8080/loop/order/ecpaycheck");
-		aioCheck.setOrderResultURL("https://eeit13428loop.southeastasia.cloudapp.azure.com:8080/loop/order/myorder");
+		aioCheck.setReturnURL("http://eeit13428loop.southeastasia.cloudapp.azure.com:8080/loop/ecpaycheck");
+		//aioCheck.setOrderResultURL("http://eeit13428loop.southeastasia.cloudapp.azure.com:8080/loop/order/myorder");
 		// 輸出畫面
 		String htmlContent = aio.aioCheckOut(aioCheck, null);
 		
